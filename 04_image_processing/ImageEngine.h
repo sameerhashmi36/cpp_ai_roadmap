0f938c4ec1d180ef5f1a4283b07b3a6165a92e91
sameerhashmi36 <sameerhashmi1995@gmail.com>
2026-06-12 16:20:35 +0200

    image processing header file C++

---
+++ 04_image_processing/ImageEngine.h
@@ -0,0 +1,23 @@
+#ifndef IMAGE_ENGINE_H
+#define IMAGE_ENGINE_H
+
+#include <opencv2/opencv.hpp>
+#include <string>
+
+// We define a clean OOP Class for managing our vision operations
+class ImageEngine {
+private:
+    // Encapsulation: The raw image frame is protected inside the class
+    cv::Mat raw_frame;
+
+public:
+    // Constructor Blueprint
+    ImageEngine();
+
+    // Safe Interface Methods (APIs)
+    void captureInput();
+    void applyBrightness(double amount);
+    void saveResult(const std::string& filename);
+};
+
+#endif // IMAGE_ENGINE_H
